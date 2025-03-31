1d4534c87e9f53ddce0fc3203d32c7e35ca9af5b
Nitesh <nitesh.e21@nsut.ac.in>
2025-03-31 18:15:58 +0530

    BEE

---
+++ Lec-10-expressRouter/model/blog.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+let blogSchema= new mongoose.Schema({
+    title:{
+        type:String,
+    },
+    content: String,
+    author:{
+        type:String,
+        require:true
+    },
+    date:{
+        type:Date,
+        default:Date.now()
+    }
+});
+module.exports=mongoose.model("Blog",blogSchema);
