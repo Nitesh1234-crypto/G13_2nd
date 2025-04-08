9a0f66082e8e4d97ab937699b714d901b761e996
Nitesh <nitesh.e21@nsut.ac.in>
2025-04-08 13:15:42 +0530

    bee

---
+++ lec-10-mongoose/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app= express();
+const mongoose = require('mongoose');
+const User = require("./model/user");
+const Blog = require("./model/blog")
+app.use(express.json());
+
+app.post("/users",async (req,res)=>{
+    const {name,email,password} = req.body;
+    let newUser=new User({name:name,email:email,password:password});
+    await newUser.save();
+    // console.log("hello");
+    res.send("user added successfully")
+
+})
+
+
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/g26mondb')
+  .then(() => console.log('Connected!'))
+  .catch((err)=>console.log(err));
+app.listen(4444,(req,res)=>{
+    console.log("server started")
+})
