0209709e7c2c380fa30084c361678633bee7caa9
Nitesh <nitesh.e21@nsut.ac.in>
2025-04-10 19:39:41 +0530

    hbs

---
+++ Lec-15-hbs/routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express= require("express");
+const router = express.Router();
+const User = require("../model/user");
+router.post("/",async(req,res)=>{
+  let {name,email,password} = req.body;
+  let newUser=new User({name:name,email:email,password:password});
+  await newUser.save(); //will add data into user collection
+  //any query to the database is asynchronous.
+  res.send("user added")
+})
+
+
+module.exports=router
