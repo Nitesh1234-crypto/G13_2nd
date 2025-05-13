ca93754795a3234fcfb180cad9d6ee13cc18d85e
Nitesh <nitesh.e21@nsut.ac.in>
2025-05-13 18:46:27 +0530

    hbs

---
+++ lec-14-hbs/server.js
@@ -0,0 +1,40 @@
+const express= require("express");
+const app = express();
+const mongoose = require('mongoose');
+const User = require("./model/user");
+const blog = require("./model/blog");
+const blogRoute= require("./routes/blogRoutes");
+const userRoute= require("./routes/userRoutes")
+// const blog = require("./model/blog");
+app.use(express.json())
+app.set('view engine', 'hbs');
+//create a route to render home page
+app.get("/home",(req,res)=>{
+  res.render("home",{
+    name:"Nitesh",
+    age:23
+  })
+})
+app.get("/blogpage",(req,res)=>{
+   res.render("blogs",{
+      blogs:[{id:1,title:"my first blog", banner:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuEv9fKeIqwR9CydwQDRXeJqACxhQtfD5oYA&s",
+        content:"dshfdshfdsfafdsfdsjfsjdfjdsfjdsjfdsnfdsnfdsjnfdsfdsjfusfurewruweurskdskjdfdfjdsfudfjdsfdsfj"
+      },
+      {id:1,title:"my second blog", banner:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuEv9fKeIqwR9CydwQDRXeJqACxhQtfD5oYA&s",
+        content:"dshfdshfdsfafdsfdsjfsjdfjdsfjdsjfdsnfdsnfdsjnfdsfdsjfusfurewruweurskdskjdfdfjdsfudffkdsfkdfjdsfdsfj"
+      }]
+   })
+})
+
+
+
+
+app.use("/blogs",blogRoute);
+app.use("/users",userRoute);
+
+mongoose.connect('mongodb://127.0.0.1:27017/g13mdb')
+  .then(() => console.log('Connected!'))
+  .catch((err)=>console.log(err))
+app.listen(5555,(req,res)=>{
+    console.log("server started")
+})
